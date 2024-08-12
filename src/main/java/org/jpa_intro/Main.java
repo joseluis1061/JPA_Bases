d04debcc69949d41deceaea08b1b6d224e907874
José Luis Zúñiga <jose@tangibledesign.co>
2024-08-12 13:57:58 -0500

    Add repository

--- src/main/java/org/jpa_intro/Main.java
+++ src/main/java/org/jpa_intro/Main.java
@@ -1,17 +1,66 @@
 package org.jpa_intro;
+// Importa la clase User definida en el paquete org.jpa_intro.enity
+import org.jpa_intro.enity.User;
+// Importa la clase UtilEntity definida en el paquete org.jpa_intro.utils
+import org.jpa_intro.utils.UtilEntity;
+// Importa la interfaz EntityManager necesaria para JPA
+import javax.persistence.EntityManager;
+// Importa la interfaz List para manejar colecciones de objetos
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
   public static void main(String[] args) {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
+    // Obtiene un EntityManager para interactuar con la base de datos
+    EntityManager em = UtilEntity.getEntityManager();
+    // Crea una consulta JPQL para obtener todos los usuarios y ejecuta la consulta
+    // e es un alias para los datos de User la segunda e indica todos los datos
+    List<User> users = em.createQuery("SELECT e from User e", User.class).getResultList();
+    System.out.println("------- Listar Datos -------");
+    // Imprime cada usuario en la lista
+    users.forEach(System.out::println);
+    System.out.println("----------------------------\n");
 
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
-    }
+    System.out.println("------- Buscar empleado por id -------");
+    int userId = 1;
+    User userByID = em.find(User.class, userId);
+    System.out.println(userByID);
+    System.out.println("----------------------------\n");
+
+    System.out.println("------- Crear nuevo usuario -------");
+    User newUser =  new User();
+    newUser.setEmail("correo10@correo.com");
+    newUser.setLogin("Logead");
+    newUser.setPassword("admin123");
+    newUser.setNickname("salesMan");
+
+    em.getTransaction().begin();
+    em.persist(newUser);
+    em.getTransaction().commit();
+    System.out.println("Nuevo empleado creado: "+ newUser);
+
+    System.out.println("----------------------------\n");
+
+    System.out.println("------- Actualizar usuario -------");
+    User updateUser =  em.find(User.class, 24);
+    updateUser.setLogin("logead");
+
+    em.getTransaction().begin();
+    em.persist(updateUser);
+    em.getTransaction().commit();
+    System.out.println("Empleado actualizado: "+ updateUser);
+
+    System.out.println("----------------------------\n");
+
+    System.out.println("------- Delete usuario -------");
+    User deleteUser =  em.find(User.class, 24);
+    em.getTransaction().begin();
+    em.remove(deleteUser);
+    em.getTransaction().commit();
+    System.out.println("Empleado borrado: "+ deleteUser);
+    System.out.println("----------------------------\n");
+
+    System.out.println("----- Terminar proceso -------");
+    em.close();
+    System.out.println("----------------------------\n");
   }
 }
--- src/main/java/org/jpa_intro/utils/UtilEntity.java
+++ src/main/java/org/jpa_intro/utils/UtilEntity.java
@@ -4,13 +4,18 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class UtilEntity {
-  private static final EntityManagerFactory entityManagerFactory =  buildEntityManagerFactory();
+  // EntityManagerFactory estático y final patron singleton
+  private static final EntityManagerFactory entityManagerFactory = buildEntityManagerFactory();
 
+  // Método privado para construir el EntityManagerFactory
   private static EntityManagerFactory buildEntityManagerFactory() {
+    // Crea y retorna un EntityManagerFactory usando la unidad de persistencia especificada
     return Persistence.createEntityManagerFactory("myPersistenceUnit");
   }
 
-  public static EntityManager getEntityManagerFactory(){
+  // Método público para obtener un nuevo EntityManager
+  public static EntityManager getEntityManager() {
+    // Crea y retorna un nuevo EntityManager usando el EntityManagerFactory
     return entityManagerFactory.createEntityManager();
   }
 
